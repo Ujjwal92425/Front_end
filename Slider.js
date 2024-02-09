51b10d105dc99f23210c46585b3725b48361cbf1
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-02-09 12:20:54 +0530

    slider js

--- Slider.js
+++ Slider.js
@@ -1,3 +1,9 @@
 const a =document.querySelector('.slider');
-//var counter = 0;
-console.log(a);
+var counter = 0;
+//console.log(a);
+a.forEach(
+    (slider,index) => 
+    {
+        slider.style.left = `${index*100}`
+}
+)
