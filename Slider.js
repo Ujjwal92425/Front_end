42361c2e556b217c3a5ea4b6918c8a49129bbbff
Ujjwal Kumar <ujjwalsps54@gmail.com>
2024-02-09 12:32:10 +0530

    slider

--- Slider.js
+++ Slider.js
@@ -4,6 +4,22 @@ var counter = 0;
 a.forEach(
     (slider,index) => 
     {
-        slider.style.left = `${index*100}`
+        slider.style.left = `${index*100} % `
 }
 )
+const Previous=() => {
+    counter--
+    sliderrun()
+}
+const Next=() {
+    counter++
+    sliderrun()
+
+}
+const sliderrun=() =>
+{
+    a.forEach (
+        (slider) =>
+        slider.style.transform=`translateX(-${counter*100}%`
+    )
+}
